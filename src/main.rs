9ab765c5089ce1f0e32cee78b191d32044c30b25
Demur Rumed <junkmute@hotmail.com>
2022-11-03 21:15:46 +0000

    Use Rust 1.65's let-else syntax

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,13 @@ use std::io::{self, Read};
 use num_bigint::BigUint;
 
 fn main() {
-	let x = if let Some(a) = env::args().nth(1) {
-		let mut f = fs::File::open(&a).expect("Failed to open file");
-		let mut num = Vec::new();
-		f.read_to_end(&mut num).expect("Failed to read number");
-		num.retain(|&x| x >= b'0' && x <= b'9');
-		BigUint::parse_bytes(&num, 10).unwrap()
-	} else {
+	let Some(a) = env::args().nth(1) else {
 		println!("NULL [filename]");
 		return;
 	};
-
-	vm::run(x, io::stdin(), io::stdout());
+	let mut f = fs::File::open(&a).expect("Failed to open file");
+	let mut num = Vec::new();
+	f.read_to_end(&mut num).expect("Failed to read number");
+	num.retain(|&x| x >= b'0' && x <= b'9');
+	vm::run(BigUint::parse_bytes(&num, 10).unwrap(), io::stdin(), io::stdout());
 }
