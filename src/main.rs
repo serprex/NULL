86490222fd1abfe56a9e9f028d6e7026d4c9cc0a
Demur Rumed <junkmute@hotmail.com>
2022-11-04 21:18:32 +0000

    Fix main to deallocate everything from setup before invoking interpreter. Also lto=thin

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,16 @@ use std::io::{self, Read};
 use num_bigint::BigUint;
 
 fn main() {
-	let Some(a) = env::args().nth(1) else {
-		println!("NULL [filename]");
-		return;
+	let x = {
+		let Some(a) = env::args().nth(1) else {
+			println!("NULL [filename]");
+			return;
+		};
+		let mut f = fs::File::open(&a).expect("Failed to open file");
+		let mut num = Vec::new();
+		f.read_to_end(&mut num).expect("Failed to read number");
+		num.retain(|&x| x >= b'0' && x <= b'9');
+		BigUint::parse_bytes(&num, 10).unwrap()
 	};
-	let mut f = fs::File::open(&a).expect("Failed to open file");
-	let mut num = Vec::new();
-	f.read_to_end(&mut num).expect("Failed to read number");
-	num.retain(|&x| x >= b'0' && x <= b'9');
-	vm::run(BigUint::parse_bytes(&num, 10).unwrap(), io::stdin(), io::stdout());
+	vm::run(x, io::stdin(), io::stdout());
 }
